c17972e85b9cf83d4752fb11979827ab7c53a60a
Becky O'Connor <becky.oconnor@snowflake.com>
2023-07-18 13:43:09 +0100

    changed_db

--- pages/1_The_7_worst_cities.py
+++ pages/1_The_7_worst_cities.py
@@ -27,7 +27,7 @@ def get_db_session():
     'ACCOUNT': st.secrets["account"],
     'user': st.secrets["username"],
     'password': st.secrets["password"],
-    'database': 'VEHICLE_ACCIDENT_DATA',
+    'database': st.secrets["VEHICLE_ACCIDENTS_PRODUCTION"],
     'warehouse': 'STREAMLIT',
     'role': st.secrets["role"],
     'schema': 'RAW'
