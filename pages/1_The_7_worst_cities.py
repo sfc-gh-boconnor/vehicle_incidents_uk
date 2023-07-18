1c77f171686b234f10d7a6261080d4cc801eabec
Becky O'Connor <becky.oconnor@snowflake.com>
2023-07-18 10:19:11 +0100

    simplified_tab_1

--- pages/1_The_7_worst_cities.py
+++ pages/1_The_7_worst_cities.py
@@ -39,6 +39,8 @@ session = get_db_session()
 
 
 
+
+
 #bring in a database table from snowflake using a snowpark dataframe
 
 @st.cache_data
@@ -51,31 +53,20 @@ def worst_cities():
 
 
 #there are some icons i wish to cache - in this case i will be utilising the snowflake icon
-@st.cache_data(ttl=3600)
+@st.cache_data
 def images_icons():
     return session.sql('''SELECT  *, GET_PRESIGNED_URL(@ICONS,RELATIVE_PATH,172800) URL FROM directory(@ICONS) ORDER BY RELATIVE_PATH''').to_pandas()
 
 #there are some pictures of vehicle locations for worst places to drive - used snowpark to locate the URLs in the database and then cached the results to memory
-@st.cache_data(ttl=3600)
+@st.cache_data
 def images_journeys():
-    return session.sql('''SELECT  *, GET_PRESIGNED_URL(@LOCATIONS,RELATIVE_PATH,172800) URL FROM directory(@LOCATIONS) ORDER BY RELATIVE_PATH''').to_pandas()
+    return session.sql('''SELECT  *, GET_PRESIGNED_URL(@IMAGES,RELATIVE_PATH,172800) URL FROM directory(@IMAGES) ORDER BY RELATIVE_PATH''').to_pandas()
 
 
 #retrieve rows of data which contain the apparant worst cities to drive!
 @st.cache_data
 def retrieve_worst_cities():
     return session.table("UK_Worst_Cities_To_Drive").sort(F.col('RANK').asc()).to_pandas()
-
-
-#cache the co ordinates of the locations in order they were ranked
-@st.cache_data
-def array():
-    return session.table("UK_Worst_Cities_To_Drive").sort('RANK').select('LATITUDE','LONGITUDE').to_pandas().to_numpy()
-
-#cache the ranked data by collecting the results from snowpark data frame
-@st.cache_data
-def array2():
-    return session.table("UK_Worst_Cities_To_Drive").sort('RANK').collect()
 
 
 #this is for the accident year drop down list - to retrieve all relevant years of accidents where locations are recorded.  
@@ -102,19 +93,10 @@ st.markdown('##### Here is a map of all the worst locations to drive, as enginee
 st.markdown('source - https://www.wessexfleet.co.uk/blog/2022/05/26/best-and-worst-uk-cities-for-driving/')
     
     #using pandas to filter the selections (this was initially retrieved via snowpark)
-
-@st.cache_data()
-def selected_array(selected):
-    return session.table("UK_Worst_Cities_To_Drive").filter((F.col('CITY')==selected)).select('LATITUDE','LONGITUDE').to_pandas().to_numpy()
-    
-@st.cache_data()
-def selected_array2(selected):
-    return session.table("UK_Worst_Cities_To_Drive").filter((F.col('CITY')==selected)).to_pandas()
-
-    
-    
+  
 #create the folium map
-m = folium.Map(location=[selected_array2(selected).iloc[0].LATITUDE,selected_array2(selected).iloc[0].LONGITUDE], zoom_start=8, tiles="openstreetmap")
+selected_row = retrieve_worst_cities()[retrieve_worst_cities()["CITY"] == selected]
+m = folium.Map(location=[selected_row.iloc[0].LATITUDE,selected_row.iloc[0].LONGITUDE], zoom_start=8, tiles="openstreetmap")
 
 trail_coordinates = session.table("UK_Worst_Cities_To_Drive").sort('RANK').select('LATITUDE','LONGITUDE').to_pandas().to_numpy()
 
@@ -138,25 +120,25 @@ for A in range (0,7):
         
     <b>STATS</b>
     <BR><BR>
-    Crashes:  {array2()[A]['CRASHES']}
+    Crashes:  {retrieve_worst_cities()['CRASHES'].iloc[A]}
     <BR>
-    Congestion Level: {array2()[A]['Congestion Level %']}
+    Congestion Level: {retrieve_worst_cities()['Congestion Level %'].iloc[A]}
     <BR>
-    Cars Per Parking Space: {array2()[A]['Cars Per Parking Space']}
+    Cars Per Parking Space: {retrieve_worst_cities()['Cars Per Parking Space'].iloc[A]}
     <BR>
-    EV Charging Points: {array2()[A]['EV Charging Points']}
+    EV Charging Points: {retrieve_worst_cities()['EV Charging Points'].iloc[A]}
     <BR>
-    Air Quality Index Score: {array2()[A]['Air Quality Index Score']}
+    Air Quality Index Score: {retrieve_worst_cities()['Air Quality Index Score'].iloc[A]}
     <BR>
-    Bus Routes: {array2()[A]['Bus Routes']}
+    Bus Routes: {retrieve_worst_cities()['Bus Routes'].iloc[A]}
     <BR>
-    Overall Score: {array2()[A]['Overall Score']}
+    Overall Score: {retrieve_worst_cities()['Overall Score'].iloc[A]}
     <BR>
     <hr>
     <p style="font-family:verdana">
-    <b>Worst Junction: </b>{array2()[A]['Bad Junction in City']} 
+    <b>Worst Junction: </b>{retrieve_worst_cities()['Bad Junction in City'].iloc[A]} 
     <BR><BR>
-    <b>Source:</b><a href ="{array2()[A].SOURCE}" target="popup"> {array2()[A].SOURCE}</a></p>
+    <b>Source:</b><a href ="{retrieve_worst_cities().SOURCE.iloc[A]}" target="popup"> {retrieve_worst_cities().SOURCE.iloc[A]}</a></p>
     <hr>
     <p style="font-family:verdana">
     <br>
@@ -164,8 +146,8 @@ for A in range (0,7):
     <br>
     <BR>
     According to the data from the Department of Data, since the year 2000 
-    there have been <font style="color:red"> {array2()[A]['ACCIDENTS']} </font> accidents.  
-    Of which, <font style="color:red"> {array2()[A]['VEHICLES']} </font> vehicles and <font style="color:red"> {array2()[A]['CASUALTIES']} </font> 
+    there have been <font style="color:red"> {retrieve_worst_cities()['ACCIDENTS'].iloc[A]} </font> accidents.  
+    Of which, <font style="color:red"> {retrieve_worst_cities()['VEHICLES'].iloc[A]} </font> vehicles and <font style="color:red"> {retrieve_worst_cities()['CASUALTIES'].iloc[A]} </font> 
     casualties were involved.
 
         
@@ -176,10 +158,10 @@ for A in range (0,7):
     <p style="font-family:verdana">
     <b> WORST DRIVING CITY INSIGHTS
     <BR><BR>
-    <b>Rank: </b>{selected_array2(selected).iloc[0].RANK}<BR><BR>
-    <img src="{images_journeys().iloc[selected_array2(selected).iloc[0].ID-1].URL}", width=100>
+    <b>Rank: </b>{selected_row.iloc[0].RANK}<BR><BR>
+    <img src="{images_journeys().iloc[selected_row.iloc[0].ID-1].URL}", width=100>
     <br><br><b>City:</b>
-    {selected_array2(selected).iloc[0].CITY}<BR><BR>
+    {selected_row.iloc[0].CITY}<BR><BR>
 
     <hr>
         
@@ -187,25 +169,25 @@ for A in range (0,7):
         
     <b>STATS</b>
     <BR><BR>
-    Crashes:  {selected_array2(selected).iloc[0].CRASHES}
+    Crashes:  {selected_row.iloc[0].CRASHES}
     <BR>
-    Congestion Level: {selected_array2(selected).iloc[0]['Congestion Level %']}
+    Congestion Level: {selected_row.iloc[0]['Congestion Level %']}
     <BR>
-    Cars Per Parking Space: {selected_array2(selected).iloc[0]['Cars Per Parking Space']}
+    Cars Per Parking Space: {selected_row.iloc[0]['Cars Per Parking Space']}
     <BR>
-    EV Charging Points: {selected_array2(selected).iloc[0]['EV Charging Points']}
+    EV Charging Points: {selected_row.iloc[0]['EV Charging Points']}
     <BR>
-    Air Quality Index Score: {selected_array2(selected).iloc[0]['Air Quality Index Score']}
+    Air Quality Index Score: {selected_row.iloc[0]['Air Quality Index Score']}
     <BR>
-    Bus Routes: {selected_array2(selected).iloc[0]['Bus Routes']}
+    Bus Routes: {selected_row.iloc[0]['Bus Routes']}
     <BR>
-    Overall Score: {selected_array2(selected).iloc[0]['Overall Score']}
+    Overall Score: {selected_row.iloc[0]['Overall Score']}
     <BR>
     <hr>
     <p style="font-family:verdana">
-    <b>Worst Junction: </b>{selected_array2(selected).iloc[0]['Bad Junction in City']} 
+    <b>Worst Junction: </b>{selected_row['Bad Junction in City'].iloc[0]} 
     <BR><BR>
-    <b>Source:</b><a href ="{selected_array2(selected).iloc[0].SOURCE}" target="popup"> {selected_array2(selected).iloc[0].SOURCE}</a></p>
+    <b>Source:</b><a href ="{selected_row.SOURCE.iloc[0]}" target="popup"> {selected_row.SOURCE.iloc[0]}</a></p>
     <hr>
     <p style="font-family:verdana">
     <br>
@@ -213,8 +195,8 @@ for A in range (0,7):
     <br>
     <BR>
     According to the data from the Department of Data, since the year 2000 
-    there have been <font style="color:red"> {array2()[A]['ACCIDENTS']} </font> accidents.  
-    Of which, <font style="color:red"> {array2()[A]['VEHICLES']} </font> vehicles and <font style="color:red"> {array2()[A]['CASUALTIES']} </font> 
+    there have been <font style="color:red"> {selected_row.ACCIDENTS.iloc[0]} </font> accidents.  
+    Of which, <font style="color:red"> {selected_row.VEHICLES.iloc[0]} </font> vehicles and <font style="color:red"> {selected_row.CASUALTIES.iloc[0]} </font> 
     casualties were involved.
 
         
@@ -223,8 +205,9 @@ for A in range (0,7):
     iframe2 = folium.IFrame(html2,width=700,height=400)
     popup = folium.Popup(iframe,max_width=700)
     popup2 = folium.Popup(iframe2,max_width=700)
-    folium.Marker(array()[A],popup=popup, icon=folium.Icon(color='blue', prefix='fa',icon='car'), icon_size=(40,40)).add_to(m)
-    folium.Marker(selected_array(selected)[0],popup=popup2, icon=folium.Icon(color='red', prefix='fa',icon=f'{selected_array2(selected).iloc[0].RANK}'), icon_size=(40, 40)).add_to(m)
+    folium.Marker(retrieve_worst_cities()[['LATITUDE', 'LONGITUDE']].iloc[A].to_numpy(),popup=popup, icon=folium.Icon(color='blue', prefix='fa',icon='car'), icon_size=(40,40)).add_to(m)
+    folium.Marker(selected_row[['LATITUDE', 'LONGITUDE']].iloc[0],popup=popup2, icon=folium.Icon(color='red', prefix='fa',icon=f'{retrieve_worst_cities()[retrieve_worst_cities()["CITY"] == selected].RANK.iloc[0]}'), icon_size=(40, 40)).add_to(m)
+    
     
     
 st_data = folium_static(m, width=1200, height= 800)
